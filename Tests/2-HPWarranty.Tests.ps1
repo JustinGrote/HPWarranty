42bacad1e55f002eda3566b2bfcf3a968b571e88
JustinGrote <justingrote+github@gmail.com>
2019-03-10 13:43:20 -0700

    Test: Separate ISEE and HPSC test Methods

--- Tests/2-HPWarranty.Tests.ps1
+++ Tests/2-HPWarranty.Tests.ps1
@@ -1,6 +1,8 @@
 Describe "HPWarranty API Operational Tests" {
     Context "Serial Number Query Test" {
         import-module .\HPWarranty -force
+
+        #These serial numbers were found on public google image searches
         $SNs = @()
         $SNs += [PSCustomObject]@{SerialNumber="MXQ84504T0";ProductNumber="459497-B21"}
         $SNs += [PSCustomObject]@{SerialNumber="MX263000LM";ProductNumber="840668-001"}
@@ -9,10 +11,14 @@ Describe "HPWarranty API Operational Tests" {
         $SNs += [PSCustomObject]@{SerialNumber="2S6206B304";ProductNumber="AP846A"}
         $SNs += [PSCustomObject]@{SerialNumber="USE62317RY";ProductNumber="403321-B21"}
 
-        #These serial numbers were found on public google image searches
-        It "Serial Number MXQ84504T0 is not covered" {
-            ($SNs[0] | Get-HPEntWarrantyEntitlement -warningaction silentlycontinue).Covered | Should Be $false
+
+        It "Serial Number MXQ84504T0 is not covered (ISEE Method)" {
+            ($SNs[0] | Get-HPEntWarrantyEntitlement -QueryMethod ISEE -warningaction silentlycontinue).Covered | Should Be $false
         }
+        It "Serial Number MXQ84504T0 is not covered (HPSC Method)" {
+            ($SNs[0] | Get-HPEntWarrantyEntitlement -QueryMethod HPSC -warningaction silentlycontinue).Covered | Should Be $false
+        }
+
         It "Serial Number MX263000LM is covered" {
             ($SNs[1] | Get-HPEntWarrantyEntitlement -warningaction silentlycontinue).Covered | Should Be $true
         }
